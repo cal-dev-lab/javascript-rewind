f51a25958cd671af1e8bd5451d53a9aebc936bfa
Callum Lees <hi@c-l.dev>
2022-01-20 23:54:56 +0000

    Created math operators (#2)

---
+++ Math Operators/math-operators.js
@@ -0,0 +1,44 @@
+/*
+
+    Author: {
+        platform: "Github",
+        user: "cal-dev-lab"
+    },
+    About: {
+        description: "Math operators"
+    }
+
+
+*/
+
+// Basic math operators
+
+// Addition
+console.log(2 + 3); // Outputs 5
+
+// Subtraction
+console.log(2 - 3); // Outputs -1
+
+// Multiplication
+console.log(2 * 3); // Outputs 6
+
+// Division
+console.log(3 / 2); // Outputs 1.5
+
+// Remainder
+console.log(12 % 5); // Outputs 2
+
+
+// Math operators
+
+// Random number
+Math.random(); // Creates random number between 0 & 1
+
+// Generate a random number between 0 & 100
+Math.random() * 100;
+
+// Round down a random number to full integer
+Math.floor(Math.random());
+
+// Round up a random number to full integer
+Math.ceil(Math.random());
